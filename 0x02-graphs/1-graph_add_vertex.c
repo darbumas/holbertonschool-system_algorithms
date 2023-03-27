0cda385ed9658fc5678134757ff1541c4d8104f0
darbumas <2314@holbertonschool.com>
2023-03-27 19:05:39 +0000

    refactor to append vertices sequentially

--- 0x02-graphs/1-graph_add_vertex.c
+++ 0x02-graphs/1-graph_add_vertex.c
@@ -9,40 +9,44 @@
  */
 vertex_t *graph_add_vertex(graph_t *graph, const char *str)
 {
-	vertex_t *vertex, *temp;
+	vertex_t *vertex, *temp, *last;
 
 	if (!graph || !str)
 		return (NULL);
-	/* First check if a vertex with same content already exist */
+
+	/* Check if a vertex with the same content already exists */
 	temp = graph->vertices;
-	while (temp)
+	while (temp != NULL)
 	{
 		if (strcmp(temp->content, str) == 0)
 			return (NULL);
+		last = temp;
 		temp = temp->next;
 	}
-	/* Allocate memory for new vertex */
+
+	/* Allocate memory for the new vertex & the content/copy of the str*/
 	vertex = malloc(sizeof(vertex_t));
 	if (!vertex)
 		return (NULL);
-	/* Allocate memory for the content and copy the string */
 	vertex->content = strdup(str);
 	if (!vertex->content)
 	{
 		free(vertex);
 		return (NULL);
 	}
-	/* Initialize new vertex */
+
+	/* Initialize the new vertex */
 	vertex->index = graph->nb_vertices;
 	vertex->nb_edges = 0;
 	vertex->edges = NULL;
 	vertex->next = NULL;
+
 	/* Update the graph */
-	if (!graph->last_v)
+	if (graph->vertices == NULL)
 		graph->vertices = vertex;
 	else
-		graph->last_v->next = vertex;
-	graph->last_v = vertex;
+		last->next = vertex;
 	graph->nb_vertices++;
+
 	return (vertex);
 }
