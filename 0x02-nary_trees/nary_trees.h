8b7473893e67d57475c518aad6634ea714aa5599
darbumas <2314@holbertonschool.com>
2023-04-19 18:49:04 +0000

    update header file for strdup

--- 0x02-nary_trees/nary_trees.h
+++ 0x02-nary_trees/nary_trees.h
@@ -1,9 +1,8 @@
 #ifndef NARY_TREES_H
 #define NARY_TREES_H
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
+#include <string.h>
 
 /**
  * struct nary_tree_s - N-ary tree node structure
